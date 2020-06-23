0a685f63e0470ed3951e5074479264ba3124983f
Martin Schaer <martin@schaerweb.com>
2020-06-23 07:43:46 -0600

    Install storybook

---
+++ .storybook/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+  stories: ['../stories/**/*.stories.js', '../src/components/**/*.stories.js'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links'
+  ],
+};
