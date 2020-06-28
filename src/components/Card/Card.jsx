152797f7f6c455d74afadbd897bd30066c4bcd2d
Martin Schaer <martin@schaerweb.com>
2020-06-28 10:43:30 -0600

    Add Card height property

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,25 +5,36 @@ import styled from 'styled-components'
 // ---------------------------------------------------------------------------------------------------------------------
 // Styled Components
 // ---------------------------------------------------------------------------------------------------------------------
+// StyledCard height property is called h so that it doesn’t appear in the resulting DOM node.
 const StyledCard = styled.div`
   background-color: white;
+  box-sizing: border-box;
   box-shadow: 0 0 1rem rgba(0, 0, 0, 0.1);
   border-radius: 0.5rem;
-  margin: 1rem;
   padding: 2rem;
+  height: ${props =>
+    props.h === 'full'
+    ? '100%'
+    : typeof props.h === 'number'
+    ? `${props.h}rem`
+    : 'auto'
+  };
 `
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Component
 // ---------------------------------------------------------------------------------------------------------------------
 const Card = ({
+  height,
   children
 }) => {
   // -------------------------------------------------------------------------------------------------------------------
   // Render
   // -------------------------------------------------------------------------------------------------------------------
   return (
-    <StyledCard>
+    <StyledCard
+      h={height}
+    >
       {children}
     </StyledCard>
   )
@@ -33,10 +44,11 @@ const Card = ({
 // PropTypes, defaults & export
 // ---------------------------------------------------------------------------------------------------------------------
 Card.propTypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
+  height: PropTypes.oneOfType([PropTypes.oneOf(['default', 'full']), PropTypes.number])
 }
 Card.defaultProps = {
-  size: 'default'
+  height: 'default'
 }
 
 export default Card
