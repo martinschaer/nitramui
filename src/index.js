12fa176b3486aa13925946f63cb7218aedd1adcf
Martin Schaer <martin@schaerweb.com>
2020-07-11 10:50:08 -0600

    Update exports

--- src/index.js
+++ src/index.js
@@ -8,5 +8,7 @@ export { modes, themes } from './components/common/designSystem'
 // ---------------------------------------------------------------------------------------------------------------------
 export { default as Card } from './components/Card'
 export { default as Pane } from './components/Pane'
+export { default as Table } from './components/Table'
 export { default as Layout } from './components/Layout'
+export { default as Container } from './components/Container'
 export { default as NitramUI } from './components/NitramUI'
