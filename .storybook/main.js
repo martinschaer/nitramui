0744c8358d538e5734ca1e5be735507f10263b67
Martin Schaer <martin@schaerweb.com>
2020-07-11 10:49:04 -0600

    Remove storybook actions addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   stories: ['../stories/**/*.stories.js', '../src/components/**/*.stories.js'],
   addons: [
-    '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-knobs/register'
   ],
