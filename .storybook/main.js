101c629ef141287fa387f15b7125d91f436ea32c
Martin Schaer <martin@schaerweb.com>
2020-07-17 08:08:13 -0600

    Add storysource y docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,8 @@ module.exports = {
   stories: ['../stories/**/*.stories.js', '../src/components/**/*.stories.js'],
   addons: [
     '@storybook/addon-links',
+    '@storybook/addon-storysource',
+    '@storybook/addon-docs',
     '@storybook/addon-knobs/register'
   ],
 };
