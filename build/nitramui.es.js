b8a2f4d2ebd1cae884a87e6f940aee7131ccaa60
Martin Schaer <martin@schaerweb.com>
2020-07-31 19:02:09 -0600

    Build

--- build/nitramui.es.js
+++ build/nitramui.es.js
@@ -4294,7 +4294,6 @@ const StyledCardHeader = styled.header`
 const StyledCardBody = styled.main`
   padding: ${props => props.noPadding ? '0' : props.compact ? '1rem' : '2rem'};
   overflow: scroll;
-  min-height: 100%;
 
   & > *:first-child {
     margin-top: 0;
